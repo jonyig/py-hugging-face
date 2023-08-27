af7907dca71eff50bc8537de4de372e638851cad
Jonny <ji394su311203106@gmail.com>
2023-08-27 17:09:14 +0800

    feat: add poc of translation and sentiment-analysis

--- main.py
+++ main.py
@@ -4,13 +4,18 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from transformers import pipeline
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-    translator = pipeline("translation", model='Helsinki-NLP/opus-mt-zh-en')  # 使用翻譯任務
-    translator('這是一個很困難的問題')
+    translator = pipeline(model='Helsinki-NLP/opus-mt-zh-en')  # 使用翻譯任務
+    result = translator(['這是一個很困難的問題',"不要"])
+    print(result)
 
+    specific_model = pipeline(model="finiteautomata/bertweet-base-sentiment-analysis")
+
+    data = ["I love you", "I hate you","can you help me to clean my pants"]
+    result = specific_model(data)
+    print(result)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
